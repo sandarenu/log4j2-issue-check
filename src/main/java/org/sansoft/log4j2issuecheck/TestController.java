6ef639d1a43d903dd9663802a2f1a72efb1f1cb8
Chathurika Sandarenu <sandarenu@gmail.com>
2021-12-14 08:00:30 +0530

    Updated logger to org.slf4j.Logger

--- src/main/java/org/sansoft/log4j2issuecheck/TestController.java
+++ src/main/java/org/sansoft/log4j2issuecheck/TestController.java
@@ -1,7 +1,7 @@
 package org.sansoft.log4j2issuecheck;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,7 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    private static Logger LOGGER = LogManager.getLogger(TestController.class);
+    /**
+     * With slf4j logger also the same issue is available
+     */
+    private static Logger LOGGER = LoggerFactory.getLogger(TestController.class);
 
     @GetMapping("/test")
     public String  testEndpoint(@RequestParam("userParam") String userParam){
